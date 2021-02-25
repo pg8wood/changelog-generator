9d02b00a01e5fb0733b9af2a94d7333b375570e0
Patrick Gatewood <pg8wood@vt.edu>
2021-02-25 00:09:40 -0500

    Add additional Publish command tests

--- Tests/changelog-generatorTests/PublishCommandTests.swift
+++ Tests/changelog-generatorTests/PublishCommandTests.swift
@@ -36,30 +36,27 @@ class PublishCommandTests: XCTestCase {
         })
     }
     
-    func test_givenValidCommand_whenChangelogsExistButNoChangelogFileExists_thenReleaseIsDocumented() {
+    func test_givenValidCommand_whenChangelogsExistButNoChangelogFileExists_thenErrorIsThrown() {
         XCTAssertNoThrow(try withTemporaryDirectory { directory in
-            try recordEntry(in: directory.asURL)
-            
-            var publishCommand = Publish()
-            publishCommand.version = "42"
-            publishCommand.releaseDate = Publish.makeDefaultReleaseDateString()
-            publishCommand.unreleasedChangelogsDirectory = directory.asURL
-            publishCommand.dryRun = false
-            publishCommand.changelogFilename = "CHANGELOG.md"
-            
-            XCTAssertThrowsError(try publishCommand.run()) { error in
-                XCTAssertEqual(error as? ChangelogError, .changelogNotFound)
+            try withTemporaryChangelogEntry(dir: directory) { _ in
+                
+                var publishCommand = Publish()
+                publishCommand.version = "42"
+                publishCommand.releaseDate = Publish.makeDefaultReleaseDateString()
+                publishCommand.unreleasedChangelogsDirectory = directory.asURL
+                publishCommand.dryRun = false
+                publishCommand.changelogFilename = "CHANGELOG.md"
+                
+                XCTAssertThrowsError(try publishCommand.run()) { error in
+                    XCTAssertEqual(error as? ChangelogError, .changelogNotFound)
+                }
             }
         })
     }
     
     func test_givenValidCommand_whenChangelogsExist_thenReleaseIsDocumented() {
         XCTAssertNoThrow(try withTemporaryDirectory { directory in
-            try withTemporaryFile(dir: directory, prefix: "fakeEntry", suffix: "md") { changelogEntryFile in
-                let changelogEntry = ChangelogEntry(type: .addition, text: "Added temporarily")
-                let changelogEntryData = try JSONEncoder().encode(changelogEntry)
-                changelogEntryFile.fileHandle.write(changelogEntryData)
-                
+            try withTemporaryChangelogEntry(dir: directory) { changelogEntry in
                 let absoluteChangelogPath = AbsolutePath(FileManager.default.currentDirectoryPath)
                 
                 try withTemporaryFile(dir: absoluteChangelogPath, prefix: "CHANGELOG", suffix: "md") { changelogFile in
@@ -69,18 +66,78 @@ class PublishCommandTests: XCTestCase {
                     publishCommand.unreleasedChangelogsDirectory = directory.asURL
                     publishCommand.dryRun = false
                     publishCommand.changelogFilename = changelogFile.path.pathString
-
+                    
                     try publishCommand.run()
+                    
+                    let changelogEntryContent = try String(contentsOf: changelogFile.path.asURL)
+                    let publishedChangelogContent = try String(contentsOf: changelogFile.path.asURL)
+                    
+                    XCTAssertFalse(publishedChangelogContent.isEmpty)
+                    XCTAssertTrue(publishedChangelogContent.contains(changelogEntryContent))
                 }
             }
         })
     }
     
-    private func recordEntry(in directory: URL) throws {
-        var log = Log()
-        log.entryType = .change
-        log.text = ["Made some additions"]
-        log.unreleasedChangelogsDirectory = directory
-        try log.run()
+    func test_givenValidCommand_whenChangelogsExist_thenUnreleasedChangelogsAreDeleted() {
+        XCTAssertNoThrow(try withTemporaryDirectory { directory in
+            func fetchUnreleasedChangelogFileCount() throws -> Int {
+                try FileManager.default.contentsOfDirectory(atPath: directory.asURL.path).count
+            }
+            
+            try withTemporaryChangelogEntry(dir: directory) { changelogEntry in
+                XCTAssertEqual(try fetchUnreleasedChangelogFileCount(), 1)
+                let absoluteChangelogPath = AbsolutePath(FileManager.default.currentDirectoryPath)
+                
+                try withTemporaryFile(dir: absoluteChangelogPath, prefix: "CHANGELOG", suffix: "md") { changelogFile in
+                    var publishCommand = Publish()
+                    publishCommand.version = "42"
+                    publishCommand.releaseDate = Publish.makeDefaultReleaseDateString()
+                    publishCommand.unreleasedChangelogsDirectory = directory.asURL
+                    publishCommand.dryRun = false
+                    publishCommand.changelogFilename = changelogFile.path.pathString
+                    
+                    try publishCommand.run()
+                    
+                    XCTAssertEqual(try fetchUnreleasedChangelogFileCount(), 0)
+                }
+            }
+        })
+    }
+    
+    func test_givenValidCommand_whenDryRunFlagIsSupplied_thenNoChangelogsAreDeleted() {
+        XCTAssertNoThrow(try withTemporaryDirectory { directory in
+            func fetchUnreleasedChangelogFileCount() throws -> Int {
+                try FileManager.default.contentsOfDirectory(atPath: directory.asURL.path).count
+            }
+            
+            try withTemporaryChangelogEntry(dir: directory) { changelogEntry in
+                let absoluteChangelogPath = AbsolutePath(FileManager.default.currentDirectoryPath)
+                
+                try withTemporaryFile(dir: absoluteChangelogPath, prefix: "CHANGELOG", suffix: "md") { changelogFile in
+                    var publishCommand = Publish()
+                    publishCommand.version = "42"
+                    publishCommand.releaseDate = Publish.makeDefaultReleaseDateString()
+                    publishCommand.unreleasedChangelogsDirectory = directory.asURL
+                    publishCommand.dryRun = true
+                    publishCommand.changelogFilename = changelogFile.path.pathString
+                    
+                    try publishCommand.run()
+                    
+                    XCTAssertEqual(try fetchUnreleasedChangelogFileCount(), 1)
+                }
+            }
+        })
+    }
+
+    
+    private func withTemporaryChangelogEntry(dir directory: AbsolutePath?, _ body: (TemporaryFile) throws -> Void) throws {
+        try withTemporaryFile(dir: directory, prefix: "fakeEntry", suffix: "md") { changelogEntryFile in
+            let changelogEntry = ChangelogEntry(type: .addition, text: "Added temporarily")
+            let changelogEntryData = try JSONEncoder().encode(changelogEntry)
+            changelogEntryFile.fileHandle.write(changelogEntryData)
+            
+            try body(changelogEntryFile)
+        }
     }
 }
