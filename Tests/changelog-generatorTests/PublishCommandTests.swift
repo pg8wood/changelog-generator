2dfa28c8e28100a2b79a2293d6de0370551f767c
Patrick Gatewood <pg8wood@vt.edu>
2022-01-21 12:21:12 -0500

    Ensure all parsable's properties are initialized in tests
    This is a precondition required by the argument parser framework

--- Tests/changelog-generatorTests/PublishCommandTests.swift
+++ Tests/changelog-generatorTests/PublishCommandTests.swift
@@ -11,14 +11,15 @@ import TSCBasic
 
 class PublishCommandTests: XCTestCase {
     func test_givenNoVersionArgument_thenThrowsError() {
-        var publishCommand = Publish()
+        var publishCommand = Publish.makeCommandWithFakeCommandLineArguments()
+        publishCommand.version = ""
         publishCommand.options = Changelog.Options(unreleasedChangelogsDirectory: Changelog.Options.defaultUnreleasedChangelogDirectory)
         
         XCTAssertThrowsError(try publishCommand.run())
     }
     
     func test_givenValidCommand_whenChangelogDirectoryDoesntExist_thenThrowsError() {
-        var publishCommand = Publish()
+        var publishCommand = Publish.makeCommandWithFakeCommandLineArguments()
         publishCommand.version = "42"
         publishCommand.options = Changelog.Options(unreleasedChangelogsDirectory: Changelog.Options.defaultUnreleasedChangelogDirectory)
         
